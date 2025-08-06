9150b908f30fc8680eadb2f75747c20de77b6e2e
dameonrs <6keisuke@k-l-s.net>
2025-08-06 18:49:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ async def daily_poster():
     global latest_message, participant_data, event_start_time, reminded_users
 
     now = datetime.datetime.now(pytz.timezone("Asia/Tokyo"))
-    if now.hour == 19 and now.minute == 10:
+    if now.hour == 19 and now.minute == 00:
         participant_data.clear()
         reminded_users.clear()
         event_start_time = now.replace(hour=21, minute=0, second=0, microsecond=0)
