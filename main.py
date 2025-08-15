89ef66457b6a6d50ce1040c940c4c914d4dd3638
dameonrs <6keisuke@k-l-s.net>
2025-08-15 21:29:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ async def post_party_embed():
 @tasks.loop(minutes=1)
 async def daily_poster():
     now = datetime.datetime.now(pytz.timezone("Asia/Tokyo"))
-    if now.hour == 21 and now.minute == 10:
+    if now.hour == 21 and now.minute == 40:
         party_sessions.clear()
         global latest_party_index
         latest_party_index = -1
